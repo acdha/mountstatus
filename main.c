1b638cf401bcdc3340a8e5bc3c318b21dc9d1df9
Chris Adams <chris@improbable.org>
2016-02-18 16:31:05 -0500

    Whitespace normalization

--- main.c
+++ main.c
@@ -47,11 +47,11 @@ int main (int argc, char const* argv[]) {
 		fprintf(stderr, "%s must be run as root\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if (argc > 1) {
 		fprintf(stderr, "%s does not accept command-line arguments\n", argv[0]);
 	}
-	
+
 	// Close all file handles:
 	fflush(NULL);
 
@@ -78,7 +78,7 @@ int main (int argc, char const* argv[]) {
 	struct sigaction sact;
 	memset(&sact, 0, sizeof sact);
 	sact.sa_handler = (void*) &kill_children;
-	sigemptyset(&sact.sa_mask); 
+	sigemptyset(&sact.sa_mask);
 
 	if (sigaction(SIGALRM, &sact, 0) < 0) {
 		syslog(LOG_ERR, "Cannot install SIGALRM handler: errno %d: %m", errno);
@@ -92,34 +92,34 @@ int main (int argc, char const* argv[]) {
 	while (1==1) {
 		check_mounts();
 		// We'll reap any zombies just in case:
-				while (waitpid(-1, &zombiestatus, WNOHANG) > 0);
+		while (waitpid(-1, &zombiestatus, WNOHANG) > 0);
 		sleep(180);
 	}
-	
+
 	closelog();
 	return(0);
 }
 
 void check_mounts() {
 	time_t startTime = time(NULL);
-	
+
 	int livemountcount = 0;
 	int mountcount = 0;
 
 #ifdef __linux__
-		FILE *fp; 
-		struct mntent *entry; 
+    FILE *fp;
+    struct mntent *entry;
 
-		fp = setmntent( _PATH_MOUNTED, "r" ); 
+    fp = setmntent( _PATH_MOUNTED, "r" );
 
-		while ((entry = getmntent(fp)) != NULL ) { 
-		if (check_mount(entry->mnt_dir)) {
-			livemountcount++;
-		}
-		mountcount++;
-		} 
+    while ((entry = getmntent(fp)) != NULL ) {
+        if (check_mount(entry->mnt_dir)) {
+            livemountcount++;
+        }
+        mountcount++;
+    }
 
-		endmntent( fp ); 
+    endmntent( fp );
 #else
 	struct statfs *mounts;
 	mountcount = getmntinfo(&mounts, MNT_NOWAIT);
@@ -141,15 +141,15 @@ void check_mounts() {
 		syslog(LOG_INFO, "Checked %u mounts in %i seconds", livemountcount, (int)(time(NULL) - startTime));
 	}
 }
-	
+
 
 bool check_mount(const char* path) {
 	child = fork();
 	if (child < 0) {
-		syslog(LOG_ERR, "Couldn't fork a child to check mountpoint %s: errno %d: %m", path, errno);
-	} else if (child == 0) {						
+        syslog(LOG_ERR, "Couldn't fork a child to check mountpoint %s: errno %d: %m", path, errno);
+	} else if (child == 0) {
 		struct stat mountstat;
-		
+
 		if (stat(path, &mountstat) != 0) {
 			syslog(LOG_INFO, "Couldn't stat mountpoint %s: errno %d: %m", path, errno);
 			exit(42);
@@ -169,49 +169,49 @@ bool check_mount(const char* path) {
 			syslog(LOG_ERR, "Couldn't setuid(%d): errno %d: %m", mountstat.st_uid, errno);
 			exit(EXIT_FAILURE);
 		}
-		
+
 		DIR* mountpoint = opendir(path);
 
 		if (!mountpoint) {
 			syslog(LOG_ERR, "Couldn't open directory %s: errno %d: %m", path, errno);
 
-			if (errno == EACCES) { 
-				exit (42); 
-			} else { 
-				exit (EXIT_FAILURE); 
+			if (errno == EACCES) {
+				exit (42);
+			} else {
+				exit (EXIT_FAILURE);
 			}
 		}
-			
+
 		int direntc = 0;
 		struct dirent *dp;
 		while ((dp = readdir(mountpoint)) != NULL) {
 			direntc++;
 		}
-			
+
 		if (closedir(mountpoint) != 0) {
 			syslog(LOG_ERR, "Couldn't close directory %s: errno %d: %m", path, errno);
 			exit(EXIT_FAILURE);
 		}
-			
+
 		exit(42);
 	} else {
 		int status = 0;
-			
+
 		alarm(60);
 		waitpid(child, &status, 0);
 		alarm(0);
 
-		if (WIFEXITED(status)) {
-			if (WEXITSTATUS(status) == 42) {
-				return true;
-			} else {
-				syslog(LOG_ERR, "Child process %i returned %i while checking %s!", child, WEXITSTATUS(status), path);
-			} 
-		} else if (WIFSIGNALED(status)) {
-			syslog(LOG_ERR, "Child process %i terminated on signal %i while checking %s!", child, WTERMSIG(status), path);
-		} else {
-			syslog(LOG_ERR, "Child process %i terminated with status %i while checking %s!", child, WEXITSTATUS(status), path);
-		}
+        if (WIFEXITED(status)) {
+            if (WEXITSTATUS(status) == 42) {
+                return true;
+            } else {
+                syslog(LOG_ERR, "Child process %i returned %i while checking %s!", child, WEXITSTATUS(status), path);
+            }
+        } else if (WIFSIGNALED(status)) {
+            syslog(LOG_ERR, "Child process %i terminated on signal %i while checking %s!", child, WTERMSIG(status), path);
+        } else {
+            syslog(LOG_ERR, "Child process %i terminated with status %i while checking %s!", child, WEXITSTATUS(status), path);
+        }
 	}
 
 	return false;
